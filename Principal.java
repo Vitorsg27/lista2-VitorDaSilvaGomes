e61e1a80d5c891b85f28bbc29d0280c69f3e17ed
Vitor Gomes <61754619+Vitorsg27@users.noreply.github.com>
2021-02-03 19:53:15 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -11,17 +11,23 @@ public class Principal {
         BigDecimal valor = new BigDecimal("400");
         String descricao = "Super mercado";
         TipoTransacao tipo = TipoTransacao.DESPESA;
+        
+        LocalDateTime dt1 = LocalDateTime.now();
+        BigDecimal valor1 = new BigDecimal("500");
+        String descricao1 = "Aluguel";
+        TipoTransacao tipo1 = TipoTransacao.DESPESA;
 
         LocalDateTime dt2 = LocalDateTime.now();
         BigDecimal valor2 = new BigDecimal("3000");
-        String descricao2 = " Salário";
+        String descricao2 = " Salario";
         TipoTransacao tipo2 = TipoTransacao.RECEITA;
 
         Orcamento supermercado = new Orcamento(dt, valor, descricao, tipo);
+        Orcamento aluguel = new Orcamento(dt1, valor1, descricao1, tipo1);
         Orcamento sal = new Orcamento(dt2, valor2, descricao2, tipo2);
 
         System.out.println(sal.toString());
-        System.out.println();
+        System.out.println(aluguel.toString());
         System.out.println(supermercado.toString());
     }
 
